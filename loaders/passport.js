ddc52d6a328c6f1e464d4dfa276cb6a87bf5fe89
gladwinch <gladwinch20@gmail.com>
2023-01-01 18:22:57 +0530

    disable redis storage

--- loaders/passport.js
+++ loaders/passport.js
@@ -1,7 +1,7 @@
 const session = require('express-session')
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
-const RedisStore = require('connect-redis')(session)
+// const RedisStore = require('connect-redis')(session)
 const redis = require('../services/redis')
 const { userDAL } = require('../modules/user')
 
@@ -22,7 +22,7 @@ const localAuthUser = async (email, password, done) => {
 module.exports = app => {
     // Use express-session middleware to store user sessions
     app.use(session({
-        store: new RedisStore({ client: redis }),
+        // store: new RedisStore({ client: redis }),
         secret: process.env.SESSION_SECRET,
         resave: true,
         cookie: {
