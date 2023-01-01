631ff7f46cbd0e6d834380830a4c3d88a2e7d4da
gladwinch <gladwinch20@gmail.com>
2023-01-01 19:37:56 +0530

    dicto static server

--- loaders/cors.js
+++ loaders/cors.js
@@ -5,21 +5,15 @@ const WEB_SERVER_HOST = process.env.WEB_SERVER_HOST || "localhost"
 const WEB_SERVER_PORT = process.env.WEB_SERVER_PORT || 3000
 
 module.exports = app => {
-    // app.use(
-    //     cors({
-    //         credentials: true,
-    //         origin: [
-    //             "http://" + WEB_SERVER_HOST + ":" + WEB_SERVER_PORT,
-    //             "https://" + WEB_SERVER_HOST + ":" + WEB_SERVER_PORT
-    //         ]
-    //     })
-    // )
-
-    // app.use(cors())
-
-    app.all('/*', function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        next();
-      })
+    app.use(
+        cors({
+            credentials: true,
+            origin: [
+                // "http://" + WEB_SERVER_HOST + ":" + WEB_SERVER_PORT,
+                // "https://" + WEB_SERVER_HOST + ":" + WEB_SERVER_PORT
+                'http://localhost:3000',
+                'https://localhost:3000'
+            ]
+        })
+    )
 }
